fead6eeac3b30ff25b1eadfbe78803ae84bdbfd1
Andrii Stepanets <ovrass@gmail.com>
2021-02-10 18:28:15 +0200

    created project with react app and added files for homepage

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import GlobalStyles from 'styles/GlobalStyles';
+import HomePage from 'components/HomePage';
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <HomePage />
+    </>
+  );
+}
+
+export default App;
