f0d6fff5d8cfe98e8dd30cfa0c0a4e45950d314b
Andrii Stepanets <ovrass@gmail.com>
2021-02-13 23:53:46 +0200

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import GlobalStyles from 'styles/global.styles';
-import DirectoryStyles from 'styles/directory.styles';
+import { Route, Switch } from "react-router-dom";
 import HomePage from 'pages/home-page';
+
+function HatsPage() {
+  return (
+    <div>
+      <h1>Hats</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <>
-      <GlobalStyles />
-      <HomePage />
-    </>
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+        <Route render={() => <h1>Page Not Found</h1>} />
+      </Switch>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import GlobalStyles from 'styles/global.styles';
 import App from 'App';
 
 import 'normalize.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <React.StrictMode>
+      <GlobalStyles />
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
