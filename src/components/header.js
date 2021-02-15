2aaa60bfa44c5ef298ad26e9cfba77a46e4b9721
Andrii Stepanets <ovrass@gmail.com>
2021-02-15 22:46:24 +0200

    Started create of Header component

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeaderBlock from 'styles/header.styles';
+
+function Header() {
+    return (
+        <HeaderBlock className="header">
+            Header
+        </HeaderBlock>
+    );
+}
+
+export default Header;
