b8aee795bae3899385908f75b04b0c96d44bb845
Andrii Stepanets <ovrass@gmail.com>
2021-03-12 12:14:05 +0200

    Fixed action types in reducer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { Route, Switch, Redirect } from "react-router-dom";
 
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+
 import { selectCurrentUser } from './redux/user/user.selectors'
+import { checkUserSession } from './redux/user/user.actions'
 
 import Header from './components/header/header.component';
 
@@ -20,7 +22,8 @@ class App extends React.Component {
 
   unsubscribeFromAuth = null;
   componentDidMount() {
-
+    const { checkUserSession } = this.props;
+    checkUserSession();
   }
 
   componentWillUnmount() {
@@ -52,4 +55,9 @@ const mapStateToProps = createStructuredSelector({
 })
 
 
-export default connect(mapStateToProps)(App);
+const mapDispatchToProps = dispatch => ({
+  checkUserSession: () => dispatch(checkUserSession())
+})
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -3,7 +3,7 @@ import { takeLatest, put, all, call } from 'redux-saga/effects';
 import userActionTypes from './user.types'
 
 
-import { auth, googleProvider, createUserProfileDocument } from '../../firebase/firebase.utils';
+import { auth, googleProvider, createUserProfileDocument, getCurrentUser } from '../../firebase/firebase.utils';
 
 import { signInSuccess, signInFailure } from './user.actions';
 
@@ -35,12 +35,26 @@ export function* signInWithGoogle() {
   }
 }
 
-export function* signInWithEmail({ payload: { password, email } }) {
+export function* signInWithEmail({ payload: { email, password } }) {
   try {
 
     const { user } = yield auth.signInWithEmailAndPassword(email, password);
 
     yield getSnapshotFromUserAuth(user)
+
+  } catch (error) {
+    yield put(signInFailure(error))
+  }
+}
+
+export function* isUserAuthenticated() {
+  try {
+
+    const userAuth = yield getCurrentUser()
+
+    if (!userAuth) return
+
+    yield getSnapshotFromUserAuth(userAuth)
 
   } catch (error) {
     yield put(signInFailure(error))
@@ -56,9 +70,14 @@ export function* onEmailSignInStart() {
   yield takeLatest(userActionTypes.EMAIL_SIGN_IN_START, signInWithEmail)
 }
 
+export function* onCheckUserSession() {
+  yield takeLatest(userActionTypes.CHECK_USER_SESSION, isUserAuthenticated)
+}
+
 export function* userSagas() {
   yield all([
     call(onGoogleSignInStart),
-    call(onEmailSignInStart)
+    call(onEmailSignInStart),
+    call(onCheckUserSession),
   ])
 }
